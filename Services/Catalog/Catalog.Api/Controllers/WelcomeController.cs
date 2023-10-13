a465a4c35c10add2f632b69c8251e65ee500b357
AbdallahAssaker <107299561+Abdallah-Assaker@users.noreply.github.com>
2023-10-13 23:03:13 +0300

    Containerize Catalog.Api

---
+++ Services/Catalog/Catalog.Api/Controllers/WelcomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class WelcomeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Welcome to Catalog API");
+    }
+}
